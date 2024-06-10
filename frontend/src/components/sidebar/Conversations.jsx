39312ebb487c7ad9ed8365276034554470749660
LasiSipsara <sipsaralasini@gmail.com>
2024-06-10 22:45:56 +0530

    2nd part completed-ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+
+export default Conversations;
+
+// const Conversations = () => {
+//   return (
+//     <div className="py-2 flex flex-col overflow-auto">
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//     </div>
+//   );
+// };
+
+// export default Conversations;
---
+++ frontend/src/pages/signup/Signup.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import GenderCheckbox from "./GenderCheckbox";
+
+const Signup = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Sign Up <span className="text-blue-500"> Talkify</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Lasini Sipsara"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Lasi123"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="*****"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="*****"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <GenderCheckbox />
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            Already have an account
+          </a>
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
+
+// const Signup = () => {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+//         <h1 className="text-3xl font-semibold text-center text-gray-300">
+//           Sign Up <span className="text-blue-500"> Talkify</span>
+//         </h1>
+//         <form>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Full Name</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Lasini Sipsara"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Username</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Lasi123"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Password</span>
+//             </label>
+//             <input
+//               type="password"
+//               placeholder="*****"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Confirm Password</span>
+//             </label>
+//             <input
+//               type="password"
+//               placeholder="*****"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <GenderCheckbox />
+//           <a
+//             href="#"
+//             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+//           >
+//             Already have an account
+//           </a>
+//           <div>
+//             <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Signup;
